a8978f9735732f619880bf6ee102f048b9c1b918
Sergey Sergyenko <sergey.sergyenko@gmail.com>
2011-12-08 21:33:03 +0300

    Small cleanup

--- lib/magic_balancer/balancer.rb
+++ lib/magic_balancer/balancer.rb
@@ -4,8 +4,8 @@ class MagicBalancer
   class << self
     def get_ip(url)
       @counter = rand(0xffff)
-      @endpoints = get_ip_list(url)
-      @endpoints[@counter % @endpoints.size]
+      @ip_list = get_ip_list(url)
+      @ip_list[@counter % @ip_list.size]
     end
 
     def get_ip_list(url)
@@ -15,11 +15,11 @@ class MagicBalancer
 
   def initialize(url)
     @counter = rand(0xffff)
-    @endpoints = MagicBalancer::get_ip_list(url)
+    @ip_list = MagicBalancer::get_ip_list(url)
   end
 
   def get_ip
     @counter += 1
-    @endpoints[@counter % @endpoints.size]
+    @ip_list[@counter % @ip_list.size]
   end
 end
